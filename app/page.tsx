f941174c2cbee248400635dc16ebbf8b98846ce6
tgzoo7xza <tgzoo7xza@gmail.com>
2026-07-18 15:31:29 +0700

    เว็บแนะนำตัวสำหรับสมัครงาน

--- app/page.tsx
+++ app/page.tsx
@@ -167,7 +167,7 @@ export default function Home() {
                 GitHub
               </a>
 
-              <a href="/resume.pdf" target="_blank" className="link-btn resume">
+              <a href="/" target="_blank" className="link-btn resume">
                 <svg viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" strokeWidth="2">
                   <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
                   <path d="M14 2v6h6M12 18v-6M9 15l3 3 3-3" />
